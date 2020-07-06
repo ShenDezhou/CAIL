83e9fb9e6e5fd382b78bbe7c56c65f9ccc53e923
ShenDezhou <bangtech@sina.com>
2020-07-06 09:58:10 +0800

    1. Fix a label parser bug.

--- CAIL2020/htfl/evaluate.py
+++ CAIL2020/htfl/evaluate.py
@@ -60,7 +60,7 @@ def get_labels_from_file(filename):
     data_frame = pandas.read_csv(filename)
     class_dic = ['买卖合同', '承包合同', '技术合同', '知识产权合同', '其他类合同', '房地产合同', '代理合同', '居间合同', '转让合同', '贸易合同', '劳动合同', '托管合同',
                  '仓储合同', '服务合同', '经营合同', '储运合同', '租赁合同', '其他合同', '运输合同', '承揽合同', '借款合同', '供用合同', '金融合同', '房产合同']
-    labels = data_frame[0].map(class_dic.index).tolist()
+    labels = data_frame['type1'].map(class_dic.index).tolist()
     return labels
 
 def eval_file(golds_file, predicts_file):
