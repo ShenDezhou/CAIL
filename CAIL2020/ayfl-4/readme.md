8ff5dd64e2eb9567383cfadc435f4a01f64d3ab9
ShenDezhou <bangtech@sina.com>
2020-09-25 13:31:02 +0800

    1. 刑民知行四分类模型，精度为98.96%。

---
+++ CAIL2020/ayfl-4/readme.md
@@ -0,0 +1,39 @@
+# Introduction
+```
+[xla:0](100) Loss=1.16723 Rate=0.07 GlobalRate=0.05 Time=Thu Sep 24 07:41:56 2020
+[xla:0](200) Loss=1.43893 Rate=4.44 GlobalRate=0.10 Time=Thu Sep 24 07:42:23 2020
+[xla:0](300) Loss=0.99112 Rate=6.50 GlobalRate=0.14 Time=Thu Sep 24 07:42:49 2020
+[xla:0](400) Loss=0.90796 Rate=6.86 GlobalRate=0.19 Time=Thu Sep 24 07:43:17 2020
+[xla:0](500) Loss=0.97639 Rate=7.10 GlobalRate=0.23 Time=Thu Sep 24 07:43:44 2020
+[xla:0](600) Loss=1.26334 Rate=7.27 GlobalRate=0.28 Time=Thu Sep 24 07:44:12 2020
+[xla:0](700) Loss=0.87083 Rate=7.31 GlobalRate=0.32 Time=Thu Sep 24 07:44:39 2020
+[xla:0](800) Loss=0.75612 Rate=7.25 GlobalRate=0.37 Time=Thu Sep 24 07:45:07 2020
+[xla:0](900) Loss=0.76049 Rate=7.42 GlobalRate=0.41 Time=Thu Sep 24 07:45:33 2020
+[xla:0](1000) Loss=0.74393 Rate=7.29 GlobalRate=0.45 Time=Thu Sep 24 07:46:01 2020
+[xla:0](1100) Loss=0.74535 Rate=7.22 GlobalRate=0.49 Time=Thu Sep 24 07:46:29 2020
+[xla:0](1200) Loss=0.74383 Rate=7.15 GlobalRate=0.53 Time=Thu Sep 24 07:46:57 2020
+[xla:0](1300) Loss=0.74376 Rate=7.31 GlobalRate=0.58 Time=Thu Sep 24 07:47:24 2020
+[xla:0](1400) Loss=0.74370 Rate=7.48 GlobalRate=0.62 Time=Thu Sep 24 07:47:50 2020
+[xla:0](1500) Loss=0.74370 Rate=7.47 GlobalRate=0.66 Time=Thu Sep 24 07:48:17 2020
+[xla:0](1600) Loss=0.74368 Rate=7.39 GlobalRate=0.70 Time=Thu Sep 24 07:48:44 2020
+[xla:0](1700) Loss=0.74367 Rate=7.44 GlobalRate=0.74 Time=Thu Sep 24 07:49:11 2020
+[xla:0](1800) Loss=0.74367 Rate=7.36 GlobalRate=0.77 Time=Thu Sep 24 07:49:38 2020
+[xla:0](1900) Loss=0.74367 Rate=7.45 GlobalRate=0.81 Time=Thu Sep 24 07:50:05 2020
+[xla:0](2000) Loss=0.74367 Rate=7.58 GlobalRate=0.85 Time=Thu Sep 24 07:50:31 2020
+[xla:0](2100) Loss=0.74367 Rate=7.58 GlobalRate=0.89 Time=Thu Sep 24 07:50:57 2020
+Finished training epoch 0
+[xla:0](0) Acc=1.00000 Rate=0.00 GlobalRate=0.00 Time=Thu Sep 24 07:51:29 2020
+[xla:0](100) Acc=0.98515 Rate=0.00 GlobalRate=0.00 Time=Thu Sep 24 07:52:04 2020
+[xla:0](200) Acc=0.98756 Rate=0.00 GlobalRate=0.00 Time=Thu Sep 24 07:52:28 2020
+[xla:0](300) Acc=0.98837 Rate=0.00 GlobalRate=0.00 Time=Thu Sep 24 07:52:51 2020
+[xla:0](400) Acc=0.99002 Rate=0.00 GlobalRate=0.00 Time=Thu Sep 24 07:53:15 2020
+[xla:0](500) Acc=0.98503 Rate=0.00 GlobalRate=0.00 Time=Thu Sep 24 07:53:38 2020
+[xla:0](600) Acc=0.98752 Rate=0.00 GlobalRate=0.00 Time=Thu Sep 24 07:54:02 2020
+[xla:0](700) Acc=0.98859 Rate=0.00 GlobalRate=0.00 Time=Thu Sep 24 07:54:26 2020
+[xla:0](800) Acc=0.98814 Rate=0.00 GlobalRate=0.00 Time=Thu Sep 24 07:54:49 2020
+[xla:0](900) Acc=0.98890 Rate=0.00 GlobalRate=0.00 Time=Thu Sep 24 07:55:13 2020
+[xla:0] Accuracy=98.95%
+Finished test epoch 0, valid=98.95
+('DONE', 98.95)
+saved model.
+```
---
+++ CAIL2020/ayfl-4/torch_server.py
@@ -0,0 +1,112 @@
+import argparse
+import logging
+import os
+from types import SimpleNamespace
+import falcon
+import pandas
+import torch
+from falcon_cors import CORS
+import json
+import waitress
+from data import Data
+from torch.utils.data import DataLoader
+from utils import load_torch_model
+from model import BertForClassification, CharCNN
+from evaluate import evaluate
+import time
+from classmerge import class_case
+from dataclean import cleanall, shortenlines
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)-18s %(message)s')
+logger = logging.getLogger()
+cors_allow_all = CORS(allow_all_origins=True,
+                      allow_origins_list=['*'],
+                      allow_all_headers=True,
+                      allow_all_methods=True,
+                      allow_credentials_all_origins=True
+                      )
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '-c', '--config_file', default='config/bert_config.json',
+    help='model config file')
+args = parser.parse_args()
+model_config=args.config_file
+
+MODEL_MAP = {
+    'bert': BertForClassification,
+    'cnn': CharCNN
+}
+
+
+class TorchResource:
+
+    def __init__(self):
+        logger.info("...")
+        # 0. Load config
+        with open(model_config) as fin:
+            self.config = json.load(fin, object_hook=lambda d: SimpleNamespace(**d))
+        if torch.cuda.is_available():
+            self.device = torch.device('cuda')
+        else:
+            self.device = torch.device('cpu')
+        # 1. Load data
+        self.data = Data(vocab_file=os.path.join(self.config.model_path, 'vocab.txt'),
+                    max_seq_len=self.config.max_seq_len,
+                    model_type=self.config.model_type, config=self.config)
+
+        # 2. Load model
+        self.model = MODEL_MAP[self.config.model_type](self.config)
+        self.model = load_torch_model(
+            self.model, model_path=os.path.join(self.config.model_path, 'model.bin'))
+        self.model.to(self.device)
+        logger.info("###")
+
+
+    def bert_classification(self,title, content):
+        logger.info('1:{}, 2:{}'.format(title, content))
+        row = {'type1': '/', 'title': title, 'content': content}
+        df = pandas.DataFrame().append(row, ignore_index=True)
+        filename = "data/{}.csv".format(time.time())
+        df.to_csv(filename, index=False, columns=['type1', 'title', 'content'])
+        test_set = self.data.load_file(filename, train=False)
+        data_loader_test = DataLoader(
+            test_set, batch_size=self.config.batch_size, shuffle=False)
+        # Evaluate
+        answer_list = evaluate(self.model, data_loader_test, self.device)
+        answer_list = [class_case[i] for i in answer_list]
+        return {"answer": answer_list}
+
+    def on_get(self, req, resp):
+        logger.info("...")
+        resp.set_header('Access-Control-Allow-Origin', '*')
+        resp.set_header('Access-Control-Allow-Methods', '*')
+        resp.set_header('Access-Control-Allow-Headers', '*')
+        resp.set_header('Access-Control-Allow-Credentials','true')
+        title = req.get_param('1', True)
+        content = req.get_param('2', True)
+        clean_title = shortenlines(title)
+        clean_content = cleanall(content)
+        resp.media = self.bert_classification(clean_title, clean_content)
+        logger.info("###")
+
+
+    def on_post(self, req, resp):
+        """Handles POST requests"""
+        resp.set_header('Access-Control-Allow-Origin', '*')
+        resp.set_header('Access-Control-Allow-Methods', '*')
+        resp.set_header('Access-Control-Allow-Headers', '*')
+        resp.set_header('Access-Control-Allow-Credentials', 'true')
+        resp.set_header("Cache-Control", "no-cache")
+        data = req.stream.read(req.content_length)
+        jsondata = json.loads(data)
+        clean_title = shortenlines(jsondata['1'])
+        clean_content = cleanall(jsondata['2'])
+        resp.media = self.bert_classification(clean_title, clean_content)
+        logger.info("###")
+
+if __name__=="__main__":
+    api = falcon.API(middleware=[cors_allow_all.middleware])
+    api.req_options.auto_parse_form_urlencoded = True
+    api.add_route('/z', TorchResource())
+    waitress.serve(api, port=58082, threads=48, url_scheme='http')
