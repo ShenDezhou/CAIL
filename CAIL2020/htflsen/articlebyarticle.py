9ed446fb12735c6d143b507c7578de916e6e3147
ShenDezhou <bangtech@sina.com>
2020-12-09 17:27:40 +0800

    1. civil code.

--- CAIL2020/htflsen/articlebyarticle.py
+++ CAIL2020/htflsen/articlebyarticle.py
@@ -12,17 +12,21 @@ class TypeArticle():
 
     def __init__(self):
         self.contract_type = []
-        with open('data/codetype-8.dic','r',encoding='utf-8') as f:
+        with open('data/type-8.dic','r',encoding='utf-8') as f:
             for line in f:
                 self.contract_type.append(line.strip())
 
+        self.general_type = []
+        with open('data/generaltype-8.dic', 'r', encoding='utf-8') as f:
+            for line in f:
+                self.general_type.append(line.strip())
+
         self.type_range_df = pandas.read_csv('data/typearticle.csv')
+        self.general_type_range_df = pandas.read_csv('data/generalarticle.csv')
         self.code_contract_df = pandas.read_csv('dataraw/civil_code_contract.csv')
         self.rouge = lawrouge.Rouge()
 
-    def get_weighted_score(self, contract_title, civilcode=None):
-        if not civilcode:
-            civilcode = self.contract_type
+    def get_weighted_score(self, contract_title, civilcode):
         civilscore = []
         for i in range(len(civilcode)):
             scores = self.rouge.get_scores([contract_title], [civilcode[i]], avg=True)
@@ -46,10 +50,25 @@ class TypeArticle():
         return articles
 
     def findArticles(self, contract_title):
-        typeindex, typedesc = self.get_weighted_score(contract_title)
+        typeindex, typedesc = self.get_weighted_score(contract_title, self.contract_type)
         type_range = self.type_range_df[self.type_range_df['type'] == typeindex + 1]
 
         for index, row in type_range.iterrows():
+            start, to = row[1], row[2]
+            print(start, to)
+            break
+
+        contract_df = self.code_contract_df[(self.code_contract_df['id'] <= to) & (self.code_contract_df['id'] >= start)]
+        articles = contract_df['desc'].to_list()
+        print(articles)
+        return articles
+
+    def findArticles_default_2(self, contract_title):
+        general_typeindex, general_typedesc = self.get_weighted_score(contract_title, self.general_type)
+
+        general_type_range = self.general_type_range_df[self.general_type_range_df['type'] == general_typeindex]
+
+        for index, row in general_type_range.iterrows():
             start, to = row[1], row[2]
             print(start, to)
             break
@@ -73,14 +92,17 @@ if __name__ == "__main__":
 
     print(gridtype)
 
-    contract_article = '1、甲方应承诺出卖车辆不存在任何权属上的法律问题和各类尚未处理完毕的道路交通安全违法行为或者交通事故；应提供车辆的使用、维修、事故、检验以及是否办理抵押登记、交纳税费、报废期限等真实情况和信息。'
+    contract_article = '3、违反本合同第四条第4款，延期交付的违约方应按延期交付天数每天人民币_________元向乙方支付违约金，并承担延误期内的风险责任。'
     for i in range(len(gridtype)):
         articles = typearticle.findArticles(gridtype[i])
-        articles_default = typearticle.findArticles_default(gridtype[i])
-        # print(articles)
+        print(articles)
         index, article = typearticle.get_weighted_score(contract_article,articles)
         print(index, article)
+
+        articles_default = typearticle.findArticles_default_2(contract_article)
+        print(articles_default)
         index, article = typearticle.get_weighted_score(contract_article, articles_default)
         print(index, article)
+        break
 
     print('FIN')
