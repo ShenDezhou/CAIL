dd6d628e7d2876cc2edacff0d1b8abd63d63dfef
ShenDezhou <bangtech@sina.com>
2020-12-15 20:36:48 +0800

    1. expose the power of machine learning.

---
+++ CAIL2020/unifyuix/default.md
@@ -0,0 +1,35 @@
+# 法宝智能
+
+# 1.合同智能
+
+## 1.1 合同金额提取
+
+* [合同金额识别v1（总）](/static/jesbv1.html)
+  
+* [合同金额识别v2（总、分）](/static/jesbv2.html)
+
+## 1.2 合同风险识别
+
+* [合同条款意图及风险条款识别](/static/fxsb.html)
+
+## 1.3 合同分类
+
+* [合同分类-17类-12层](/static/htfl-17-12.html)
+* [合同条款-17类-24层](/static/htfl-17-24.html)
+
+
+# 2.法规
+
+## 2.1 智能检索
+* [智能分词](/static/cbfc.html)
+* [检索分词](/static/cbfcs.html)(构建索引）
+
+
+## 2.2 自动拆分
+* [法规条拆分](/static/ftcf.html)(如:民法典等法规）
+
+# 3.司法案例
+
+* [案例分类-2类](/static/alfl-2.html)
+* [案例分类-4类3层](/static/alfl-4-3.html)
+* [案例分类-4类24层](/static/alfl-4-24.html)
---
+++ CAIL2020/unifyuix/torch_server.py
@@ -0,0 +1,74 @@
+import argparse
+import logging
+
+import falcon
+
+from falcon_cors import CORS
+import json
+import waitress
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)-18s %(message)s')
+logger = logging.getLogger()
+cors_allow_all = CORS(allow_all_origins=True,
+                      allow_origins_list=['*'],
+                      allow_all_headers=True,
+                      allow_all_methods=True,
+                      allow_credentials_all_origins=True
+                      )
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '-p', '--port', default=58100,
+    help='falcon server port')
+
+args = parser.parse_args()
+
+
+class RootResource:
+
+    def __init__(self):
+        logger.info("...")
+
+    def on_get(self, req, resp, filename="default.html"):
+        # do some sanity check on the filename
+
+        resp.status = falcon.HTTP_200
+        resp.content_type = 'text/html'
+        with open(filename, 'r', encoding='utf-8') as f:
+            resp.body = f.read()
+
+class BinaryResource:
+
+    def __init__(self):
+        logger.info("...")
+
+
+    def on_get(self, req, resp, filename):
+        # do some sanity check on the filename
+
+        resp.status = falcon.HTTP_200
+        resp.content_type = 'image/png'
+        with open(filename, 'rb') as f:
+            resp.body = f.read()
+
+class TorchResource:
+
+    def __init__(self):
+        logger.info("...")
+
+
+    def on_get(self, req, resp, filename):
+        # do some sanity check on the filename
+
+        resp.status = falcon.HTTP_200
+        resp.content_type = 'text/html'
+        with open(filename, 'r', encoding='utf-8') as f:
+            resp.body = f.read()
+
+if __name__=="__main__":
+    api = falcon.API(middleware=[cors_allow_all.middleware])
+    api.req_options.auto_parse_form_urlencoded = True
+    api.add_route('/', RootResource())
+    api.add_route('/img/{filename}', BinaryResource())
+    api.add_route('/static/{filename}', TorchResource())
+    waitress.serve(api, port=args.port, threads=48, url_scheme='http')
