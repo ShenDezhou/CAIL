7699c29d8ddd392de848fa621d9597452f4b5954
ShenDezhou <bangtech@sina.com>
2020-12-30 14:48:03 +0800

    1. use abstract and fulltext as baseline.

---
+++ CAIL2020/stsb/data/makekb_meta_abstract.py
@@ -0,0 +1,44 @@
+import itertools
+import json
+import pandas
+
+
+types = ['Education', 'Awards', 'Organization|Brand', 'Food', 'Software', 'Biological', 'Game', 'Software|Game', 'Medicine', 'Person|Other', 'Law&Regulation', 'Natural&Geography', 'Constellation', 'Location|Other', 'Person', 'Time&Calendar', 'Event|Work', 'Event', 'Location|Organization', 'Location', 'Work', 'Other', 'Culture', 'Brand', 'Brand|Organization', 'Disease&Symptom', 'Person|VirtualThings', 'Website', 'Vehicle', 'VirtualThings', 'Diagnosis&Treatment', 'Organization']
+cn_types = ['教育','奖项','组织|品牌','食品','软件','生物','游戏','软件|游戏','医学','人|其他','法律法规' ,'自然地理','星座','位置|其他','人员','时间和日历','事件|工作','事件','位置|组织','位置','工作','其他' ,'文化','品牌','品牌|组织','疾病与症状','人|虚拟事物','网站','车辆',' 虚拟事物','诊断与治疗','组织']
+type_dic = dict(zip(types,cn_types))
+
+count = 0
+collect = []
+with open('kb.json','r',encoding='utf-8') as f:
+    for line in f:
+        item = json.loads(line.strip())
+        word_id = item['subject_id']
+        word = item['subject']
+        data = item['data']
+        type = item['type']
+        type = type_dic[type]
+        abstract = [predictor['object'] for predictor in data if predictor['predicate'] in ['摘要','义项描述'] and predictor['predicate'] !="。"]
+        minor_abstract = [predictor['object'] for predictor in data if predictor['predicate'] not in ['摘要', '义项描述']]
+
+        core_abstract = ""
+        if len(abstract):
+            for ab in abstract:
+                core_abstract += ab
+
+        other_abstract = ""
+        if len(minor_abstract):
+            for ab in minor_abstract:
+                other_abstract += ab
+
+        alias = item['alias']
+        collect.append({'id':word_id,'word':word, 'type':type, 'abstract': core_abstract, 'fulltext': other_abstract})
+
+        for alias_ in alias:
+            collect.append({'id': word_id, 'word': alias_, 'type':type, 'abstract': core_abstract, 'fulltext': other_abstract})
+        count +=1
+        if count % 100 == 0:
+            print('.')
+
+df = pandas.DataFrame(collect)
+df.to_csv('kb_full.csv', index=False)
+print('FIN')
--- CAIL2020/stsb/data/type_candiates.py
+++ CAIL2020/stsb/data/type_candiates.py
@@ -3,7 +3,7 @@ import pandas
 
 
 types = ['Education', 'Awards', 'Organization|Brand', 'Food', 'Software', 'Biological', 'Game', 'Software|Game', 'Medicine', 'Person|Other', 'Law&Regulation', 'Natural&Geography', 'Constellation', 'Location|Other', 'Person', 'Time&Calendar', 'Event|Work', 'Event', 'Location|Organization', 'Location', 'Work', 'Other', 'Culture', 'Brand', 'Brand|Organization', 'Disease&Symptom', 'Person|VirtualThings', 'Website', 'Vehicle', 'VirtualThings', 'Diagnosis&Treatment', 'Organization']
-
+cn_types = ['教育','奖项','组织|品牌','食品','软件','生物','游戏','软件|游戏','医学','人|其他','法律法规' ,'自然地理','星座','位置|其他','人员','时间和日历','事件|工作','事件','位置|组织','位置','工作','其他' ,'文化','品牌','品牌|组织','疾病与症状','人|虚拟事物','网站','车辆',' 虚拟事物','诊断与治疗','组织']
 typelist = set()
 with open('kb.json','r',encoding='utf-8') as f:
     for line in f:
